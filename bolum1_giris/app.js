b13f47e226e3867d549e8760e398c468e36f7f62
sekercih <hkkskrc@gmail.com>
2021-05-19 23:36:26 +0300

    javascrtstart

---
+++ bolum1_giris/app.js
@@ -0,0 +1,27 @@
+console.log("Hello Javascript");
+console.log("merhaba java");
+console.log("merhaba python");
+console.log("Javasprit çok kollsy");
+
+console.error("hatalı bir durum");
+console.warn("Burda işler normal");
+// alert("Dikkat yol geliyor");
+
+// **! ==========DEĞİŞKEN TANIMLAMA===========
+var pi = 3.14;
+console.log(pi);
+console.log(typeof pi);
+
+pi = "3";
+console.log(pi);
+console.log(typeof pi);
+
+isim = "Ahmet";
+console.log(isim);
+
+console.log(typeof isim);
+isim = 444;
+console.log(typeof isim);
+
+const kare = 566;
+console.log(kare);
